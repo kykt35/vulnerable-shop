f5117facf2b5c59c1a686c4d48df149485b9cca8
kykt35 <kykt35@gmail.com>
2026-03-10 14:56:07 +0900

    test: cover image attachment traversal flow

--- apps/shop/test/server.test.js
+++ apps/shop/test/server.test.js
@@ -25,17 +25,26 @@ async function run() {
   const tempRoot = createTempDir();
   const databasePath = path.join(tempRoot, "data", "shop.db");
   const filesDir = path.join(tempRoot, "files");
+  const sampleDir = path.join(filesDir, "samples");
+  const srcDir = path.join(tempRoot, "src");
 
-  fs.mkdirSync(filesDir, { recursive: true });
+  fs.mkdirSync(sampleDir, { recursive: true });
+  fs.mkdirSync(srcDir, { recursive: true });
   fs.writeFileSync(
-    path.join(tempRoot, "traversal-secret.txt"),
-    "directory traversal fixture\n",
+    path.join(sampleDir, "coffee.svg"),
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 40"><text x="8" y="24">coffee</text></svg>\n',
+    "utf8"
+  );
+  fs.writeFileSync(
+    path.join(srcDir, "server.js"),
+    "sensitive server source fixture\n",
     "utf8"
   );
 
   const started = startServer({
     attackerUrl: "http://localhost:9000",
     databasePath: databasePath,
+    filesRoot: filesDir,
     logger: null,
     port: 0,
     host: "127.0.0.1"
@@ -60,17 +69,18 @@ async function run() {
     assert.ok(loginResponse.headers["set-cookie"]);
 
     const cookie = loginResponse.headers["set-cookie"][0].split(";")[0];
-    const commentResponse = await sendRequest(started.server, {
+    const imageCommentResponse = await sendRequest(started.server, {
       method: "POST",
       path: "/products/1/comments",
       headers: {
         cookie: cookie
       },
       body: {
-        body: "<script>alert('xss')</script>"
+        body: "sample image",
+        image_path: "samples/coffee.svg"
       }
     });
-    assert.strictEqual(commentResponse.statusCode, 302);
+    assert.strictEqual(imageCommentResponse.statusCode, 302);
 
     const productResponse = await sendRequest(started.server, {
       path: "/products/1",
@@ -80,7 +90,63 @@ async function run() {
     });
     assert.strictEqual(productResponse.statusCode, 200);
     assert.notStrictEqual(
-      productResponse.body.indexOf(Buffer.from("<script>alert('xss')</script>")),
+      productResponse.body.indexOf(Buffer.from("/comment-images/1")),
+      -1
+    );
+
+    const imageResponse = await sendRequest(started.server, {
+      path: "/comment-images/1"
+    });
+    assert.strictEqual(imageResponse.statusCode, 200);
+    assert.notStrictEqual(
+      imageResponse.body.indexOf(Buffer.from("<svg")),
+      -1
+    );
+
+    const traversalResponse = await sendRequest(started.server, {
+      method: "POST",
+      path: "/products/1/comments",
+      headers: {
+        cookie: cookie
+      },
+      body: {
+        body: "read source",
+        image_path: "../../src/server.js"
+      }
+    });
+    assert.strictEqual(traversalResponse.statusCode, 302);
+
+    const leakedResponse = await sendRequest(started.server, {
+      path: "/comment-images/2"
+    });
+    assert.strictEqual(leakedResponse.statusCode, 200);
+    assert.notStrictEqual(
+      leakedResponse.body.indexOf(Buffer.from("sensitive server source fixture")),
+      -1
+    );
+
+    const xssCommentResponse = await sendRequest(started.server, {
+      method: "POST",
+      path: "/products/1/comments",
+      headers: {
+        cookie: cookie
+      },
+      body: {
+        body: "<script>alert('xss')</script>",
+        image_path: ""
+      }
+    });
+    assert.strictEqual(xssCommentResponse.statusCode, 302);
+
+    const xssResponse = await sendRequest(started.server, {
+      path: "/products/1",
+      headers: {
+        cookie: cookie
+      }
+    });
+    assert.strictEqual(xssResponse.statusCode, 200);
+    assert.notStrictEqual(
+      xssResponse.body.indexOf(Buffer.from("<script>alert('xss')</script>")),
       -1
     );
   } finally {
