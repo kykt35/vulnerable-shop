71faf87c7c04b30829e412aed435f896f6f546a1
kykt35 <kykt35@gmail.com>
2026-03-10 18:12:52 +0900

    test: capture clickjacking documentation expectations

--- apps/shop/test/server.test.js
+++ apps/shop/test/server.test.js
@@ -158,7 +158,15 @@ async function run() {
     });
     assert.strictEqual(handsOnResponse.statusCode, 200);
     assert.notStrictEqual(
-      handsOnResponse.body.indexOf(Buffer.from("体験⑤ ディレクトリ・トラバーサル")),
+      handsOnResponse.body.indexOf(Buffer.from("体験⑤ クリックジャッキング")),
+      -1
+    );
+    assert.notStrictEqual(
+      handsOnResponse.body.indexOf(Buffer.from("http://localhost:9000/clickjacking")),
+      -1
+    );
+    assert.notStrictEqual(
+      handsOnResponse.body.indexOf(Buffer.from("体験⑦ ディレクトリ・トラバーサル")),
       -1
     );
   } finally {
